79089e2f9c78495051be736521fd5aaf850b9e1c
Ajnas M <ajnasaju3@gmail.com>
2026-03-13 23:21:40 +0530

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Header } from "./header/header";
+import { User } from "./user/user";
+import { DUMMY_USERS } from './dummy-users';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('first-angular-app');
+  users=DUMMY_USERS;
+  onSelectUser(id: string){
+    console.log('Selected user with id'+ id);
+  }
 }
